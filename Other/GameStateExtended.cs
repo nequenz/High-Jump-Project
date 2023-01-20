d3ffb506176e7d7aa401ae79f134451d70a33471
Slava <70316676+nequenz@users.noreply.github.com>
2023-01-20 22:22:49 +0300

    Add files via upload

---
+++ Other/GameStateExtended.cs
@@ -0,0 +1,10 @@
+﻿
+
+
+public static class GameStateExtended
+{
+    public static void AttachTransition<T>(this IState me, TransitionType type, TransitionPredicate predicate)
+    {
+        me.AttachTransition(new Transition(typeof(T), type, predicate));
+    }
+}
